705d8a51643fac34b6ae6242df5c0ba4387d2a52
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-08-28 16:16:45 -0400

    Update cors.js

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,5 +1,5 @@
 function cors(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://notexchange.shop/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://nexnotesapp.herokuapp.com/');
     res.setHeader('Access-Control-Allow-Methods',    'OPTIONS, GET, POST, PUT, PATCH, DELETE');
   
     res.setHeader('Access-Control-Allow-Headers',  
