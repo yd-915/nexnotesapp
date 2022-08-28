55ddcefafb5ea424e9999a4e25d31ec571ed15b8
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-08-28 10:56:15 -0400

    Update messagesData.js

--- client/src/services/messagesData.js
+++ client/src/services/messagesData.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:5000';
+const baseUrl = ' https://notexchange.shop/';
 
 export async function createChatRoom(receiver, message) {
     return (await fetch(`${baseUrl}/messages/createChatRoom`, {
