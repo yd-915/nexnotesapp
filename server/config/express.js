7688df0de41627f96a26a67f2a13e5204fc98781
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-08-28 16:03:14 -0400

    Update express.js

--- server/config/express.js
+++ server/config/express.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 // const auth = require('../middlewares/auth');
 const cors = require('cors');
 module.exports = (app) => {
-    app.use(cors({origin: 'https://notexchange.shop/', credentials: true}));
+    app.use(cors({origin: 'https://nexnotesapp.herokuapp.com/', credentials: true}));
     app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ limit: '50mb', extended: true }));
     app.use(cookieParser());
